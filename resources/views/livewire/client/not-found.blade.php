8aac05fbf1361c8a61e80041bc70aac90bd4f627
Boudissa Ihab <boudissa.ihab@gmail.com>
2022-03-08 02:25:13 +0100

    404 error page

---
+++ resources/views/livewire/client/not-found.blade.php
@@ -0,0 +1,22 @@
+<div>
+    <section class="hero-section error-section">
+        <div class="bg-wrap hero-section_bg">
+            <div class="bg" data-bg="{{ asset('img/client/grey-background.jpg') }}"></div>
+        </div>
+        <div class="container">
+            <div class="error-wrap">
+                <h2>404</h2>
+                <p>Oups ! La page que vous recherchez semble introuvable</p>
+                <div class="clearfix"></div>
+                <form action="#" class="custom-form">
+                    <input name="se" id="se2" type="text" class="search" placeholder="Rechercher..">
+                    <button class="search-submit color-bg" id="submit_btn2"><i class="fas fa-search"></i> </button>
+                </form>
+                <div class="clearfix"></div>
+                <a href="{{ route('home') }}" class="btn color-btn flat-btn">
+                    <span>Retourner vers l'accueil</span> <i class="fas fa-caret-right"></i>
+                </a>
+            </div>
+        </div>
+    </section>
+</div>
