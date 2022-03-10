cb5668f5c1bcecfad39a23f05c353a63e3ec622d
Boudissa Ihab <boudissa.ihab@gmail.com>
2022-03-10 03:33:47 +0100

    "about us" page in client interface

---
+++ resources/views/livewire/client/about-us.blade.php
@@ -0,0 +1,46 @@
+<div>
+    <div class="breadcrumbs-header fl-wrap">
+        <div class="container">
+            <div class="breadcrumbs-header_url">
+                <a href="{{ route('home') }}">Accueil</a>
+                <span>À propos</span>
+            </div>
+            <div class="scroll-down-wrap">
+                <div class="mousey">
+                    <div class="scroller"></div>
+                </div>
+                <span>Faire défiler vers le bas</span>
+            </div>
+        </div>
+        <div class="pwh_bg"></div>
+    </div>
+    <!-- section end -->
+    <!--section -->
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="section-title sect_dec">
+                        <h2>Journal PECAN</h2>
+                        <h4>Qui sommes-nous ?</h4>
+                    </div>
+                    <div class="about-wrap">
+                        <p> @setting('about_us') </p>
+                        {{-- <a href="https://vimeo.com/225223404" class="btn float-btn color-btn image-popup"> <span>Our Story Video</span> <i class="fas fa-caret-right"></i></a> --}}
+                    </div>
+                </div>
+                {{-- <div class="col-md-1"></div>
+                <div class="col-md-6">
+                    <div class="about-img fl-wrap">
+                        <img src="{{ asset('img/client/all/1.jpg') }}" class="respimg" alt="">
+                        <div class="about-img-hotifer color-bg">
+                            <p>Your website is fully responsive so visitors can view your content from their choice of device.</p>
+                            <h4>Mark Antony</h4>
+                            <h5>Gmag CEO</h5>
+                        </div>
+                    </div>
+                </div> --}}
+            </div>
+        </div>
+    </section>
+</div>
