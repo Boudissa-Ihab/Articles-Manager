1b077c61477aa92e3c09d9676b6b3a6ca703b31f
Boudissa Ihab <boudissa.ihab@gmail.com>
2022-03-10 03:48:02 +0100

    add client routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -44,6 +44,10 @@ class RouteServiceProvider extends ServiceProvider
                 ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
 
+            Route::middleware('web')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/web.php'));
+
             Route::prefix('admin')
                 ->middleware('admin')
                 ->namespace($this->namespace)
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,16 @@
 <?php
 
+use App\Http\Livewire\Client\AboutUs;
+use App\Http\Livewire\Client\AuthorDetails;
+use App\Http\Livewire\Client\Authors;
+use App\Http\Livewire\Client\ContactUs;
+use App\Http\Livewire\Client\DocumentDetails;
+use App\Http\Livewire\Client\Documents;
+use App\Http\Livewire\Client\DomainDetails;
+use App\Http\Livewire\Client\Domains;
+use App\Http\Livewire\Client\Home;
+use App\Http\Livewire\Client\NotFound;
+use App\Http\Livewire\Client\TermsConditions;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -14,4 +25,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', Home::class)->name('home');
+Route::get('/contact-us', ContactUs::class)->name('contact-us');
+Route::get('/about-us', AboutUs::class)->name('about-us');
+Route::get('/terms-and-conditions', TermsConditions::class)->name('terms-and-conditions');
 
+Route::get('/authors', Authors::class)->name('authors');
+Route::get('/authors/{author}', AuthorDetails::class)->name('authors.author');
+
+Route::get('/documents', Documents::class)->name('documents');
+Route::get('/documents/{document}', DocumentDetails::class)->name('documents.document-details');
+
+Route::get('/domains', Domains::class)->name('domains');
+Route::get('/domains/{domain}', DomainDetails::class)->name('domains.domain');
+
+Route::get('/not-found', NotFound::class)->name('not-found');
